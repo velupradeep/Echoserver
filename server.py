3f76a352fe262dface605418b17c159ac2c4b897
velupradeep <pradeep29072006@gmail.com>
2025-05-05 22:46:13 +0530

    success

--- server.py
+++ server.py
@@ -1,30 +1,3 @@
-# Echoserver
-Echo server and client using python socket
-
-# AIM:
-
-To develop a simple webserver to serve html programming pages.
-
-## DESIGN STEPS:
-
-### Step 1:
-
-Design of echo server and client using python socket
-
-### Step 2:
-
-Implementation using Python code
-
-### Step 3:
-
-Testing the server and client 
-
-## PROGRAM:
-
-
-## SERVER:
-
-```
 import socket
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
@@ -58,33 +31,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 print(f"Error receiving/sending data: {e}")
                 exit()
 
-```
-
-
-
-
-## CLIENT:
-
-```
-import socket
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-
-print(f"Received {data!r}")
-
-```
-
-## OUTPUT:
-
-## SERVER SIDE:
-![alt text](server.png)
-
-
-## CLIENT SIDE:
-![alt text](client.png)
-## RESULT:
-The program is executed successfully
